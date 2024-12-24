2d191ed2e11a67869dc2b32ec6a7e3490fb25a68
Dmitry <40324321+inilim@users.noreply.github.com>
2024-12-25 00:11:43 +0300

    Update function.php

--- function.php
+++ function.php
@@ -2,7 +2,11 @@
 
 if (!\function_exists('DI')) {
     /**
-     * 
+     * @template T of object
+     * @param class-string $dep
+     * @param null|class-string|object|mixed[] $args_or_context array is args else context
+     * @param null|class-string|object $context
+     * @return T
      */
     function DI(string $dep, $args_or_context = null, $context = null)
     {
